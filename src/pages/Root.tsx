8e9afff2ed648b9fb7bc941f3e1b024d7d712949
Wafaa <Wafaabahgat>
2024-08-29 00:55:05 +0300

    .

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../components/header/Header";
-import Container from "../components/Container";
 
 const Root = () => {
   return (
     <div className="flex flex-col">
-      
         <Header />
         <Outlet />
     </div>
